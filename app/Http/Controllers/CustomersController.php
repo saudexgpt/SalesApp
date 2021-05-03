0cfeb177706cf87af2e73d64806bd461eae67c93
sammy4best <saudexgpt@gmail.com>
2021-05-03 13:39:15 +0100

    Update 03-05-2021

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Customer;
+use App\Models\CustomerContact;
 use Illuminate\Http\Request;
 
 class CustomersController extends Controller
@@ -25,7 +26,7 @@ class CustomersController extends Controller
             }
         }
         $customer_type_id = $request->customer_type_id;
-        $customers = Customer::with(['customerType', 'tier', 'subRegion', 'region', 'registrar', 'assignedOfficer', 'verifier', 'payments.confirmer', 'payments.transaction.staff' => function ($q) {
+        $customers = Customer::with(['customerContacts', 'customerType', 'tier', 'subRegion', 'region', 'registrar', 'assignedOfficer', 'verifier', 'payments.confirmer', 'payments.transaction.staff' => function ($q) {
             $q->orderBy('id', 'DESC');
         }, 'transactions'])->where($condition)->orderBy('id', 'DESC')->paginate(20);
         return response()->json(compact('customers'), 200);
@@ -38,33 +39,53 @@ class CustomersController extends Controller
      */
     public function store(Request $request)
     {
-        $user = $this->getUser();
-        $customer = Customer::where('business_name', $request->business_name)->first();
+        $unsaved_customers = json_decode(json_encode($request->unsaved_customers));
+        $customer_list = [];
+        foreach ($unsaved_customers as $unsaved_customer) {
+            $user = $this->getUser();
+            $customer = Customer::where('business_name', $unsaved_customer->business_name)->first();
 
-        // we fetch the geo information of the given address
-        list($lat, $long, $formatted_address, $street, $area) = $this->getLocation($request->address);
-        if (!$customer) {
-            $customer = new Customer();
-            $customer->customer_type_id = $request->customer_type_id;
-            $customer->tier_id = $request->tier_id;
-            $customer->sub_region_id = $request->sub_region_id;
-            $customer->region_id = $request->region_id;
-            $customer->business_name = $request->business_name;
-            $customer->email = $request->email;
-            $customer->phone1 = $request->phone1;
-            $customer->phone2 = $request->phone2;
-            $customer->address = $formatted_address;
-            $customer->street = $street;
-            $customer->area = $area;
-            $customer->longitude = $long;
-            $customer->latitude = $lat;
-            $customer->registered_by = $user->id;
-            $customer->relating_officer = $user->id;
-            $customer->save();
+            // we fetch the geo information of the given address
+            list($lat, $long, $formatted_address, $street, $area) = $this->getLocation($unsaved_customer->address);
+            if (!$customer) {
+                $contacts = json_decode(json_encode($unsaved_customer->customer_contacts));
+                $customer = new Customer();
+                $customer->customer_type_id = $unsaved_customer->customer_type_id;
+                $customer->tier_id = $unsaved_customer->tier_id;
+                $customer->sub_region_id = $unsaved_customer->sub_region_id;
+                $customer->region_id = $unsaved_customer->region_id;
+                $customer->business_name = $unsaved_customer->business_name;
+                $customer->email = $unsaved_customer->email;
+                // $customer->phone1 = $unsaved_customer->phone1;
+                // $customer->phone2 = $unsaved_customer->phone2;
+                $customer->address = $formatted_address;
+                $customer->street = $street;
+                $customer->area = $area;
+                $customer->longitude = $long;
+                $customer->latitude = $lat;
+                $customer->registered_by = $user->id;
+                $customer->relating_officer = $user->id;
+                if ($customer->save()) {
+                    if (count($contacts) > 0) {
+                        foreach ($contacts as $contact) {
+                            $new_contact = new CustomerContact();
+                            $new_contact->customer_id = $customer->id;
+                            $new_contact->name = $contact->name;
+                            $new_contact->phone1 = $contact->phone1;
+                            $new_contact->phone2 = $contact->phone2;
+                            $new_contact->role = $contact->role;
+                            $new_contact->save();
+                        }
+                    }
 
-            return $this->show($customer);
+                    $customer_list[] = $this->show($customer);
+                }
+                // Generate notification before returning ///////////////////////
+                // Write notification code here////////////////////////////
+
+            }
         }
-        return response()->json(['message' => 'Customer Exists']);
+        return response()->json(['customers' => $customer_list, 'message' => 'success'], 200);
     }
 
     private function getLocation($address)
@@ -109,10 +130,10 @@ class CustomersController extends Controller
      */
     public function show(Customer $customer)
     {
-        $customer = $customer::with(['customerType', 'tier', 'subRegion', 'region', 'registrar', 'assignedOfficer', 'verifier', 'payments.confirmer', 'payments.transaction.staff' => function ($q) {
+        $customer = $customer::with(['customerContacts', 'customerType', 'tier', 'subRegion', 'region', 'registrar', 'assignedOfficer', 'verifier', 'payments.confirmer', 'payments.transaction.staff' => function ($q) {
             $q->orderBy('id', 'DESC');
-        }, 'transactions'])->first();
-        return response()->json(compact('customer'), 200);
+        }, 'transactions'])->find($customer->id);
+        return $customer;
     }
 
     /**
