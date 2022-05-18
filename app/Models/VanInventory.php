a7a5393b9ab3304704493123a28b300b386618d1
sammy4best <saudexgpt@gmail.com>
2022-05-18 02:52:42 +0100

    testing

--- app/Models/VanInventory.php
+++ app/Models/VanInventory.php
@@ -21,7 +21,7 @@ class VanInventory extends Model
         return $this->belongsTo(SubInventory::class, 'sub_inventory_id', 'id');
     }
 
-    private function deductFromVanInventory($item_id, $quantity)
+    public function deductFromVanInventory($item_id, $quantity)
     {
         $user = $this->getUser();
         $van_inventories = VanInventory::where(['staff_id' => $user->id, 'item_id' => $item_id])->where('balance', '>', 0)->get();
